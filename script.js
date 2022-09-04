61ea11848e1a36b186eba95e7bed30c0d7805982
zex3d <omk14p@gmail.com>
2022-09-05 03:43:52 +0530

    push all

--- script.js
+++ script.js
@@ -1,35 +1,69 @@
+// navigation bar code and active(old)
+// function show(Page_id, nav_id) {
+//     var x = document.getElementById("content-container").getElementsByClassName("one");
+//     var y = document.getElementById("h_nav").getElementsByTagName("a")
+//     for (i = 0; i < x.length; i++) {
+//         if (x[i].id === Page_id) {
+//             document.getElementById(Page_id).style.display = "block";
+
+//         } else {
+
+//             document.getElementById(x[i].id).style.display = "none";
+//         }
+//     }
+//     document.getElementById("h_nav").classList.toggle('fade')
+//     for (i = 0; i < y.length; i++) {
+//         if (y[i].id === nav_id) {
+//             if (!document.getElementById(nav_id).classList.contains("active")) {
+//                 document.getElementById(nav_id).classList.add("active")
+//             }
+//         } else {
+//             document.getElementById(y[i].id).classList.replace("active", "act")
+//         }
+//     }
+// }
+
+// new scroll
+// function scrolldiv() {
+//     var elem = document.getElementById("home");
+//     elem.scrollIntoView();
+// }
+
+
 // navigation bar code and active
-function show(Page_id, nav_id) {
+function pid(Page_id) {
     var x = document.getElementById("content-container").getElementsByClassName("one");
-    var y = document.getElementById("h_nav").getElementsByTagName("a")
     for (i = 0; i < x.length; i++) {
         if (x[i].id === Page_id) {
-            document.getElementById(Page_id).style.display = "block";
-
-        } else {
-
-            document.getElementById(x[i].id).style.display = "none";
-        }
-    }
-    document.getElementById("h_nav").classList.toggle('fade')
-    for (i = 0; i < y.length; i++) {
-        if (y[i].id === nav_id) {
-            if (!document.getElementById(nav_id).classList.contains("active")) {
-                document.getElementById(nav_id).classList.add("active")
-            }
-        } else {
-            document.getElementById(y[i].id).classList.replace("active", "act")
+            document.getElementById(Page_id).scrollIntoView();
         }
     }
 }
-
+function nid(nav_id) {
+    var y = document.getElementById("h_nav").getElementsByTagName("a");
+    for (i = 0; i < y.length; i++) {
+        if (y[i].id === nav_id) {
+            if (!document.getElementById(nav_id).classList.contains("active")) {
+                document.getElementById(nav_id).classList.add("active");
+                console.log(nav_id);
+            }
+        } else {
+            document.getElementById(y[i].id).classList.replace("active", "act");
+        }
+    }
+}
+function show(Page_id, nav_id) {
+    pid(Page_id);
+    document.getElementById("h_nav").classList.toggle('fade')
+    nid(nav_id);
+}
 //mobile navigation
 var toggle1 = document.getElementById("id3");
 toggle1.onclick = function () {
     document.getElementById("h_nav").classList.toggle('fade');
 }
 //home page
-show("lorem", "_home_link")
+show("home", "_home_link")
 
 //loading screen
 var loader = document.getElementById('loader');
@@ -54,8 +88,7 @@ toggle.onclick = function () {
     document.documentElement.setAttribute('data-theme', targetTheme)
     localStorage.setItem('theme', targetTheme);
 }
-// new scroll
-function scrolldiv() {
-    var elem = document.getElementById("Lorem");
-    elem.scrollIntoView();
-}
+//new dynamic padding while scrolling
+const navht=document.querySelector('#header').offsetHeight;
+console.log(navht);
+documentElement.style.setProperty('--scrolltm',navht);
