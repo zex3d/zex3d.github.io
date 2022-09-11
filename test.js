7eaf1de294e74f5fe357cf3ccc375dbf1ddca2e4
zex3d <omk14p@gmail.com>
2022-09-12 01:44:33 +0530

    same as prevois

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
+//loading screen
+var loader = document.getElementById('loader');
+var content = document.getElementById('container');
+
+function ready() {
+    loader.classList.toggle('fade');
+    content.classList.toggle('fade');
+    loader.style.display = "none";
+}
 //scroll active setting up 
 const links = document.querySelectorAll('.links');
 const sections = document.querySelectorAll('section');
@@ -10,3 +19,32 @@ function changeLinkState() {
 
 // changeLinkState();
 window.addEventListener('scroll', changeLinkState());
+
+// navigation bar code and active
+function pid(Page_id) {
+    var x = document.getElementById("container").getElementsByClassName("one");
+    for (i = 0; i < x.length; i++) {
+        if (x[i].id === Page_id) {
+            document.getElementById(Page_id).scrollIntoView();
+        }
+    }
+}
+// function nid(nav_id) {
+//     var y = document.getElementById("h_nav").getElementsByTagName("a");
+//     for (i = 0; i < y.length; i++) {
+//         if (y[i].id === nav_id) {
+//             if (!document.getElementById(nav_id).classList.contains("active")) {
+//                 document.getElementById(nav_id).classList.add("active");
+//             }
+//         } else {
+//             document.getElementById(y[i].id).classList.replace("active", "act");
+//         }
+//     }
+// }
+function show(Page_id, nav_id) {
+    pid(Page_id);
+    document.getElementById("h_nav").classList.toggle('fade')
+    // nid(nav_id);
+}
+//home page-default page
+show("home", "_home_link")
