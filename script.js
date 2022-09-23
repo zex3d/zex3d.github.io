703e01501098e01911913c4e74fb6bd7e59f0adb
zex3d <omk14p@gmail.com>
2022-09-24 01:25:33 +0530

    all clear except animation

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
+function id(id) {
+    return document.getElementById(id)
+}
 // navigation bar code and active
 function pid(Page_id) {
-    var x = document.getElementById("content-container").getElementsByClassName("one");
+    var x = id("content-container").getElementsByClassName("one");
     for (i = 0; i < x.length; i++) {
         if (x[i].id === Page_id) {
-            document.getElementById(Page_id).scrollIntoView();
+            id(Page_id).scrollIntoView();
         }
     }
 }
 function nid(nav_id) {
-    var y = document.getElementById("h_nav").getElementsByTagName("a");
+    var y = id("h_nav").getElementsByTagName("a");
     for (i = 0; i < y.length; i++) {
         if (y[i].id === nav_id) {
-            if (!document.getElementById(nav_id).classList.contains("active")) {
-                document.getElementById(nav_id).classList.add("active");
+            if (!id(nav_id).classList.contains("active")) {
+                id(nav_id).classList.add("active");
             }
         } else {
-            document.getElementById(y[i].id).classList.replace("active", "act");
+            id(y[i].id).classList.replace("active", "act");
         }
     }
 }
@@ -28,21 +31,24 @@ function show(Page_id, nav_id) {
 //home page
 show("home", "_home_link")
 
+
 //loading screen
-var loader = document.getElementById('loader');
-var content = document.getElementById('content-container');
+var loader = id('loader');
+var content = id('content-container');
 
 function ready() {
     loader.classList.toggle('fade');
     content.classList.toggle('fade');
     loader.style.display = "none";
 }
+
 // night dark and light mode preferance
 function cool(y, mode) {
     for (i = 0; i < y.length; i++) {
-        y[i].id === mode ? document.getElementById(y[i].id).style.display = "block" : document.getElementById(y[i].id).style.display = "none";
+        y[i].id === mode ? id(y[i].id).style.display = "block" : id(y[i].id).style.display = "none";
     }
 }
+
 //scroll active setting up 
 const links = document.querySelectorAll('.links');
 const sections = document.querySelectorAll('section');
@@ -54,14 +60,17 @@ function changeLinkState() {
 }
 changeLinkState();
 window.addEventListener('scroll', changeLinkState);
+
 //^mode switcher
-var toggle = document.getElementById("_dark_link");
-const y = document.getElementById("_dark_link").getElementsByTagName("svg");
+var toggle = id("_dark_link");
+const y = id("_dark_link").getElementsByTagName("svg");
 var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+
 //default 
 if (storedTheme) {
     storedTheme === "dark" ? cool(y, "moon") : cool(y, "sun")
 }
+
 //toggle
 document.documentElement.setAttribute('data-theme', storedTheme)
 toggle.onclick = function () {
@@ -76,18 +85,49 @@ toggle.onclick = function () {
     document.documentElement.setAttribute('data-theme', targetTheme)
     localStorage.setItem('theme', targetTheme);
 }
+
 //new dynamic padding while scrolling
 const navht = document.querySelector('#header').offsetHeight;
 documentElement.style.setProperty('--scrolltm', navht);
+
 //mobile navigation for menu
 function openNav() {
-    document.getElementById("h_nav").style.left = "0%";
-    document.getElementById("open").style.display = "none";
-    document.getElementById("close").style.display = "flex";
+    id("h_nav").style.left = "0%";
+    id("open").style.display = "none";
+    id("close").style.display = "flex";
 }
 
 function closeNav() {
-    document.getElementById("h_nav").style.left = "100%";
-    document.getElementById("close").style.display = "none";
-    document.getElementById("open").style.display = "flex";
+    id("h_nav").style.left = "100%";
+    id("close").style.display = "none";
+    id("open").style.display = "flex";
+}
+
+//time-Project
+function time(id1) {
+
+    id(id1).classList.toggle('active');
+    var z = id('clcode').getElementsByTagName('span');
+    var v = 0;
+    var list = [];
+    for (i = 0; i < z.length; i++) {
+        (id(z[i].id).classList.contains('active')) ? list.push(z[i].id) : '';
+    }
+    const element = id('time').getElementsByClassName('container');
+    for (j = 0; j < element.length; j++) {
+        (element[j].classList.contains("right")) ? element[j].classList.remove('right') : '';
+        (element[j].classList.contains("left")) ? element[j].classList.remove('left') : '';
+        var flag = "F";
+        const x = element[j].getAttribute("class").toString();
+        for (i = 0; i < list.length; i++) {
+            (x.includes(list[i])) ? flag = "T" : '';
+        }
+        if (flag === "T") {
+            (element[j].style.display = "block");
+            v += 1;
+            (v % 2 == 0) ? element[j].classList.add("right") : element[j].classList.add("left");
+        } else {
+            (element[j].style.display = "none");
+        }
+    }
 }
