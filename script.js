ef59d988a11df6255b0663703015fce83f6c3174
zex3d <omk14p@gmail.com>
2022-09-24 21:11:29 +0530

    push

--- script.js
+++ script.js
@@ -140,30 +140,41 @@ function hidelist(x) {
 }
 //swipe function
 //start
-var start = null;
-window.addEventListener("touchstart", function (event) {
-    if (event.touches.length === 1) {
-        //just one finger touched
-        start = event.touches.item(0).clientX;
-    } else {
-        //a second finger hit the screen, abort the touch
-        start = null;
-    }
-});
-//end
-window.addEventListener("touchend", function (event) {
-    var offset = 50;//at least 100px are a swipe
-    if (start) {
-        //the only finger that hit the screen left it
-        var end = event.changedTouches.item(0).clientX;
+// var start = null;
+// window.addEventListener("touchstart", function (event) {
+//     if (event.touches.length === 1) {
+//         start = event.touches.item(0).clientX;
+//     } else {
+//         //a second finger hit the screen, abort the touch
+//         start = null;
+//     }
+// });
+// //end
+// window.addEventListener("touchend", function (event) {
+//     var offset = 50;//at least 100px are a swipe
+//     if (start) {
+//         //the only finger that hit the screen left it
+//         var end = event.changedTouches.item(0).clientX;
+//         if (end > start + offset) {
+//             //a left -> right swipe
+//             openNav()
+//         }
+//         if (end < start - offset) {
+//             //a right -> left swipe
+//             closeNav()
+//         }
+//     }
+// });
 
-        if (end > start + offset) {
-            //a left -> right swipe
-            openNav()
-        }
-        if (end < start - offset) {
-            //a right -> left swipe
-            closeNav()
-        }
-    }
-});
+let touchstartX=0
+let touchendX=0
+function chkdir(){
+    (touchstartX>touchendX)?alert('r'):alert('l')
+}
+document.addEventListener('touchstart',e=>{
+    touchstartX=e.changedTouches[0].screenX
+})
+document.addEventListener('touchend',e=>{
+    touchendX=e.changedTouches[0].screenX
+    chkdir()
+})
