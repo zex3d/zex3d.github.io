b657f8a41417046eca9c6faf47c5a0649a2f6eed
zex3d <omk14p@gmail.com>
2022-09-24 21:44:42 +0530

    push

--- script.js
+++ script.js
@@ -139,22 +139,6 @@ function hidelist(x) {
     }
 }
 //swipe function
-//start
-// var start = null;
-// window.addEventListener("touchstart", function (event) {
-//     if (event.touches.length === 1) {
-//         start = event.touches.item(0).clientX;
-//     } else {
-//         //a second finger hit the screen, abort the touch
-//         start = null;
-//     }
-// });
-// //end
-// window.addEventListener("touchend", function (event) {
-//     var offset = 50;//at least 100px are a swipe
-//     if (start) {
-//         //the only finger that hit the screen left it
-//         var end = event.changedTouches.item(0).clientX;
 //         if (end > start + offset) {
 //             //a left -> right swipe
 //             openNav()
@@ -162,9 +146,6 @@ function hidelist(x) {
 //         if (end < start - offset) {
 //             //a right -> left swipe
 //             closeNav()
-//         }
-//     }
-// });
 
 let touchstartX=0
 let touchendX=0
